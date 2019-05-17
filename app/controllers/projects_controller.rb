2e901b695619ba026b14c52362547850f850a7b4
kostakis88 <kostasdallas2010@hotmail.com>
2019-05-17 23:48:44 +0300

    slugged url and edit destroy

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,13 +21,29 @@ class ProjectsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @project.update project_params
+      redirect_to @project, notice: "That project was updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
   private
 
   def find_project
-    @project = Project.find(params[:id])
+    @project = Project.friendly.find(params[:id])
   end
 
   def project_params
-    params.require(:project).permit(:title, :description, :link)
+    params.require(:project).permit(:title, :description, :link, :slug)
   end
 end
